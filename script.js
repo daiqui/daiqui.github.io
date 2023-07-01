6a01b038cba99f21798a51ad54ee2d0f6ea11617
daiqui <137735994+daiqui@users.noreply.github.com>
2023-07-01 17:56:40 +0200

    formatting code

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const checkBalanceButton = document.getElementById('checkBalanceButton');
-  const balanceResult = document.getElementById('balanceResult');
+document.addEventListener("DOMContentLoaded", function () {
+  const checkBalanceButton = document.getElementById("checkBalanceButton");
+  const balanceResult = document.getElementById("balanceResult");
 
-  checkBalanceButton.addEventListener('click', async function () {
-    if (typeof window.ethereum !== 'undefined') {
-      await ethereum.request({ method: 'eth_requestAccounts' });
-      const accounts = await ethereum.request({ method: 'eth_accounts' });
+  checkBalanceButton.addEventListener("click", async function () {
+    if (typeof window.ethereum !== "undefined") {
+      await ethereum.request({ method: "eth_requestAccounts" });
+      const accounts = await ethereum.request({ method: "eth_accounts" });
 
       if (accounts.length > 0) {
         const address = accounts[0];
         const balance = await getBalance(address);
         balanceResult.textContent = `Balance: ${balance} BNB`;
       } else {
-        balanceResult.textContent = 'No accounts found.';
+        balanceResult.textContent = "No accounts found.";
       }
     } else {
-      balanceResult.textContent = 'Please install MetaMask.';
+      balanceResult.textContent = "Please install MetaMask.";
     }
   });
 
@@ -25,10 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
     );
     const data = await response.json();
 
-    if (data.status === '1') {
+    if (data.status === "1") {
       return data.result / 1e18; // Convert balance from wei to BNB
     } else {
-      throw new Error('Failed to retrieve balance.');
+      throw new Error("Failed to retrieve balance.");
     }
   }
 });
