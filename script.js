53a8bc49236d05250d44c5884898a2851f08d83c
daiqui <137735994+daiqui@users.noreply.github.com>
2023-07-04 10:23:58 +0200

    simple commit2

--- script.js
+++ script.js
@@ -1,34 +1,90 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const checkBalanceButton = document.getElementById("checkBalanceButton");
-  const balanceResult = document.getElementById("balanceResult");
+// Taskliste
+let tasks = [];
 
-  checkBalanceButton.addEventListener("click", async function () {
-    if (typeof window.ethereum !== "undefined") {
-      await ethereum.request({ method: "eth_requestAccounts" });
-      const accounts = await ethereum.request({ method: "eth_accounts" });
+// DOM-Elemente abrufen
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
 
-      if (accounts.length > 0) {
-        const address = accounts[0];
-        const balance = await getBalance(address);
-        balanceResult.textContent = `Balance: ${balance} BNB`;
-      } else {
-        balanceResult.textContent = "No accounts found.";
-      }
-    } else {
-      balanceResult.textContent = "Please install MetaMask.";
-    }
-  });
+// Tasks laden, wenn die Seite geladen ist
+window.addEventListener("DOMContentLoaded", loadTasks);
 
-  async function getBalance(address) {
-    const response = await fetch(
-      `https://api-testnet.bscscan.com/api?module=account&action=balance&address=${address}&tag=latest&apikey=YOUR_API_KEY`
-    );
-    const data = await response.json();
-
-    if (data.status === "1") {
-      return data.result / 1e18; // Convert balance from wei to BNB
-    } else {
-      throw new Error("Failed to retrieve balance.");
-    }
+// Task hinzufügen
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    const task = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+      starred: false,
+    };
+    tasks.push(task);
+    saveTasks();
+    displayTask(task);
+    taskInput.value = "";
   }
-});
+}
+
+// Task anzeigen
+function displayTask(task) {
+  const taskItem = document.createElement("li");
+  taskItem.className = "task-item" + (task.completed ? " completed" : "");
+  taskItem.innerHTML = `
+    <input type="checkbox" onchange="toggleTask(${task.id})" ${
+    task.completed ? "checked" : ""
+  }>
+    <span>${task.text}</span>
+    <span class="star-icon" onclick="toggleStar(${task.id})">${
+    task.starred ? "★" : "☆"
+  }</span>
+    <button onclick="deleteTask(${task.id})">Löschen</button>
+  `;
+  taskList.appendChild(taskItem);
+}
+
+// Task abhaken/entabhalten
+function toggleTask(id) {
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+  if (taskIndex !== -1) {
+    tasks[taskIndex].completed = !tasks[taskIndex].completed;
+    saveTasks();
+    displayTasks();
+  }
+}
+
+// Task als wichtig markieren/entmarkieren
+function toggleStar(id) {
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+  if (taskIndex !== -1) {
+    tasks[taskIndex].starred = !tasks[taskIndex].starred;
+    saveTasks();
+    displayTasks();
+  }
+}
+
+// Task löschen
+function deleteTask(id) {
+  tasks = tasks.filter((task) => task.id !== id);
+  saveTasks();
+  displayTasks();
+}
+
+// Tasks speichern
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Tasks laden
+function loadTasks() {
+  const savedTasks = localStorage.getItem("tasks");
+  if (savedTasks) {
+    tasks = JSON.parse(savedTasks);
+    displayTasks();
+  }
+}
+
+// Tasks anzeigen
+function displayTasks() {
+  taskList.innerHTML = "";
+  tasks.forEach((task) => displayTask(task));
+}
