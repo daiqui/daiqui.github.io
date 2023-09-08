2016fc642a0ea36a14e5b8331bd7b8168a274a83
daiqui <137735994+daiqui@users.noreply.github.com>
2023-09-08 22:07:43 +0200

    css done

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//JavaScript
+/* nav-menu */
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
