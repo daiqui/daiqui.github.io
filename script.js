ba32f04610142c1de3625a957f100d9f95563257
daiqui <137735994+daiqui@users.noreply.github.com>
2023-09-14 14:37:39 +0200

    update to change content on the fly

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-/* NEW: nav-menu */
+const contentContainer = document.getElementById("content");
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
+// Define routes and their corresponding content
+const routes = {
+    "/": "Home",
+    "/about": "About",
+    "/contact": "Contact",
+};
+
+function loadContent(route) {
+    const content = routes[route];
+    if (content !== undefined) {
+        contentContainer.innerHTML = `<h1>${content}</h1>`;
+    } else {
+        contentContainer.innerHTML = "<h1>Page not found</h1>";
+    }
+}
+
+function navigateTo(route) {
+    window.history.pushState({}, route, route);
+    loadContent(route);
+}
+
+// Initial load based on the current URL
+loadContent(window.location.pathname);
+
+// Handle navigation when links are clicked
+document.querySelectorAll(".nav-link").forEach((link) => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault(); // Prevent default link behavior (page reload)
+        const route = link.getAttribute("data-route");
+        navigateTo(route);
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    });
+});
+
+// Handle hamburger menu click
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
-/* END:: nav-menu */
+// Handle back/forward navigation using browser history
+window.addEventListener("popstate", () => {
+    loadContent(window.location.pathname);
+});
