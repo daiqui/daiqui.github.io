897008fb7732a4af13cb138a866d0f633d700ad4
daiqui <137735994+daiqui@users.noreply.github.com>
2023-09-18 02:42:34 +0200

    set footer absolut to bottom, nav/ header fixed

--- modules/main-controller.js
+++ modules/main-controller.js
@@ -47,7 +47,7 @@ function loadContent() {
             contentMain.innerHTML = '<h1>Willkommen zum Buchklub</h1><p>Das ist der Inhalt des Buchklubs</p>';
             break;
         case '#overview':
-            contentMain.innerHTML = '<h1>Übersicht</h1><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p><h1>At vero eos et accusam et</h1><p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>';
+            contentMain.innerHTML = '<h1>Übersicht</h1><p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p><br><h1>At vero eos et accusam et</h1><p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>';
             break;
         case '#literature':
             contentMain.innerHTML = '<h1>Literature</h1><p>Explore our recommended books.</p>';
